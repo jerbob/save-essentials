d729ba5a53a4d29072001a9be986516e3b06b854
Jeremiah Boby <jerbob17@student.loreto.ac.uk>
2018-06-01 19:41:20 +0000

    Added .mp4 video saving functionality!

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ import requests
 from bs4 import BeautifulSoup
 
 
+video_pattern = re.compile('"(https://[^,]*?mp4[^,]*?)"')
+
 base_url = 'https://essentials.joincyberdiscovery.com{}'
 module_url = 'https://essentials.joincyberdiscovery.com/course/module/{}'
 
@@ -48,12 +50,39 @@ with requests.Session() as session:
         # Strip domain from URL, to save file structure
         filename = url.replace(
             'https://essentials.joincyberdiscovery.com/', ''
-        ) + '.html'
+        ) + '.'
         # Create structure if non-existent
         if not os.path.exists(os.path.dirname(filename)):
             os.makedirs(os.path.dirname(filename))
         content = session.get(url).content
+        soup = BeautifulSoup(content, 'html.parser')
+        video_iframe = soup.find('iframe')
+
+        if video_iframe:
+            # Create HTML element to replace iframe
+            new_video = (
+                '<video width="1100" height="619" controls>'
+                '<source src="{}" type="video/mp4">'
+                '</video>'
+            ).format(filename.replace('course/module/1/section/', '') + 'mp4')
+            new_video = BeautifulSoup(new_video, 'html.parser')
+            # Fake the referer header with the Essentials URL
+            session.headers['Referer'] = url
+            print('Parsing embedded video {}...'.format(filename))
+            video_src = session.get(video_iframe.attrs['src']).content.decode()
+            with open('log.html', 'w+') as file:
+                file.write(video_src)
+            video_src = video_pattern.search(video_src).group(1)
+            # Get actual video file content
+            video = session.get(video_src).content
+            # Write video file with name of section
+            print('Writing embedded video {}...'.format(filename))
+            with open(filename + 'mp4', 'wb') as file:
+                file.write(video)
+            # Replace the original iframe with the modified tag
+            video_iframe.replace_with(new_video)
+
         # Open and save the HTML page
         print('Writing section {}...'.format(filename))
-        with open(filename, 'wb') as file:
-            file.write(content)
+        with open(filename + 'html', 'w+') as file:
+            file.write(str(soup))
